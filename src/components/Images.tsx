168fde3d3c5bad569b6490170a6caff8dd206085
howsmyanimeprofilepicture <howsmyanimeprofilepicture@gmail.com>
2022-09-18 12:28:34 +0900

    feat: 코드셀로 인해 출력되는 이미지에 클래스네임할당
    * codeOutputImg라는 클래스네임을 할당

--- src/components/Images.tsx
+++ src/components/Images.tsx
@@ -1,14 +1,18 @@
 import CardMedia from "@mui/material/CardMedia";
-
-
+import React from "react";
+// <CardMedia
+//     component="img"
+//     image={image}
+//     alt="Image Ouput "
+//     key={`${i}`}
+// />
 
 export default function Images(props: { images: string[] }) {
-    return (<>{
-        props.images.map((image: string, i: number) => (<CardMedia
-            component="img"
-            image={image}
-            alt="Image Ouput "
+    return (<p>{
+        props.images.map((image: string, i: number) => (<img
+            src={image}
             key={`${i}`}
-        />))
-    }</>);
+            className="codeOutputImg"
+            alt="Image Output" />))
+    }</p>);
 }
